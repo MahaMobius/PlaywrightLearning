a4e32892af362f8b551856c4390b0c764b199a05
MahaMobius <mahalakshmiu2212@gmail.com>
2026-07-08 16:41:12 +0530

    added code

--- tests/Amazon.spec.js
+++ tests/Amazon.spec.js
@@ -8,7 +8,10 @@ test('Amazon add to cart verification', async ({page})=>{
     await expect.page.locator('[class="a-size-base a-spacing-small a-spacing-top-small a-text-normal"]').toContainText('results for');
    await page.keyboard.press('Enter');
 //git pull oee cchange
-
+let name1="Mahalakshmi";
+let name2="sarath";
+let concat=name1.concat(name2);
+console.log(concat);
     //git checking
 })
 
